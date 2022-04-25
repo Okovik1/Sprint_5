31ba4bd6c9e52e4ef429523b25966caaabfa8bc2
Viktoriia <okovik1@gmail.com>
2022-04-25 19:25:47 +0300

    added additional tests, round 2

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -14,9 +14,9 @@ public class Account {
     public boolean checkNameToEmboss(String name) {
 
 //Попыталась сделать через RegEx, в целом заработало, только не смогла побороть ограничение с 3 до 19 символов-ограничение почему то выходит до 24 в итоге:)
-//
+// Был дубль 2 с RegEx но что то не взлетело, думаю позаниматься еще ими на досуге, и добить уже позже
 //        try {
-//            Pattern pattern = Pattern.compile("^[a-zA-Z]{3,19}[\\s][a-zA-z]{3,19}$");
+//            Pattern pattern = Pattern.compile("^[a-zA-Z][{1}\\s][a-zA-z]$");
 //            Matcher matcher = pattern.matcher(name);
 //            return matcher.matches();
 //        } catch (NullPointerException exception) {
@@ -25,32 +25,29 @@ public class Account {
 //        }
 //    }
 
-
-        try {
-            String checkName = name.toLowerCase();
-            if (checkName.length() < 3) {
-                System.out.println("Name is too short");
-                return checkResult = false;
-            } else if (checkName.length() > 19) {
-                System.out.println("Name is too long");
-                return checkResult = false;
-            } else if (checkName.startsWith(" ")) {
-                System.out.println("Please, remove whitespace at the start");
-                return checkResult = false;
-            } else if (checkName.endsWith(" ")) {
-                System.out.println("Please, remove whitespace at the end");
-                return checkResult = false;
-            } else if (!checkName.contains(" ")) {
-                System.out.println("You should use whitespaces between name and surname");
-                return checkResult = false;
-            } else if (countWhitespaces(checkName) > 1) {
-                System.out.println("Name should have only 1 whitespace between firstname and lastname");
-                return checkResult = false;
-            }
-        } catch (NullPointerException e) {
-            System.out.println("Null detected rise your hands!");
+        if (name == null) {
+            System.out.println("Null detected!");
+            return checkResult = false;
+        } else if (name.length() < 3) {
+            System.out.println("Name is too short");
+            return checkResult = false;
+        } else if (name.length() > 19) {
+            System.out.println("Name is too long");
+            return checkResult = false;
+        } else if (name.startsWith(" ")) {
+            System.out.println("Please, remove whitespace at the start");
+            return checkResult = false;
+        } else if (name.endsWith(" ")) {
+            System.out.println("Please, remove whitespace at the end");
+            return checkResult = false;
+        } else if (!name.contains(" ")) {
+            System.out.println("You should use whitespaces between name and surname");
+            return checkResult = false;
+        } else if (countWhitespaces(name) > 1) {
+            System.out.println("Name should have only 1 whitespace between firstname and lastname");
             return checkResult = false;
         }
+
         return checkResult;
     }
 
